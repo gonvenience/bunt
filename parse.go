ea9d422315eea591dfde84bb8c660bdd8cd9e888
Matthias Diester <matthias.diester@de.ibm.com>
2020-11-15 21:44:51 +0100

    Add missing SGR colors
    The parse function did support the base colors and the 256 colors set.
    
    Add default colors in parse switch section.
    
    Introduce the 256 colors palette for `ESC[38;5;#m` sequences.

--- parse.go
+++ parse.go
@@ -117,27 +117,132 @@ func parseSelectGraphicRenditionEscapeSequence(escapeSeq string) (uint64, error)
 		case 4: // underline
 			result |= underlineMask
 
-		case 38: // foreground color
-			if i+4 >= len(values) {
-				return 0, fmt.Errorf("insufficient data to parse RGB foreground color")
-			}
+		case 30: // Black
+			result |= fgRGBMask(0, 0, 0)
 
-			result |= fgMask
-			result |= uint64(values[i+2]) << 8
-			result |= uint64(values[i+3]) << 16
-			result |= uint64(values[i+4]) << 24
-			i += 4
+		case 31: // Red
+			result |= fgRGBMask(170, 0, 0)
+
+		case 32: // Green
+			result |= fgRGBMask(0, 170, 0)
+
+		case 33: // Yellow
+			result |= fgRGBMask(229, 229, 16)
+
+		case 34: // Blue
+			result |= fgRGBMask(0, 0, 170)
+
+		case 35: // Magenta
+			result |= fgRGBMask(170, 0, 170)
+
+		case 36: // Cyan
+			result |= fgRGBMask(0, 170, 170)
+
+		case 37: // White
+			result |= fgRGBMask(229, 229, 229)
+
+		case 90: // Bright Black (Gray)
+			result |= fgRGBMask(85, 85, 85)
+
+		case 91: // Bright Red
+			result |= fgRGBMask(255, 85, 85)
+
+		case 92: // Bright Green
+			result |= fgRGBMask(85, 255, 85)
+
+		case 93: // Bright Yellow
+			result |= fgRGBMask(255, 255, 85)
+
+		case 94: // Bright Blue
+			result |= fgRGBMask(85, 85, 255)
+
+		case 95: // Bright Magenta
+			result |= fgRGBMask(255, 85, 255)
+
+		case 96: // Bright Cyan
+			result |= fgRGBMask(85, 255, 255)
+
+		case 97: // Bright White
+			result |= fgRGBMask(255, 255, 255)
+
+		case 40: // Black
+			result |= bgRGBMask(0, 0, 0)
+
+		case 41: // Red
+			result |= bgRGBMask(170, 0, 0)
+
+		case 42: // Green
+			result |= bgRGBMask(0, 170, 0)
+
+		case 43: // Yellow
+			result |= bgRGBMask(229, 229, 16)
+
+		case 44: // Blue
+			result |= bgRGBMask(0, 0, 170)
+
+		case 45: // Magenta
+			result |= bgRGBMask(170, 0, 170)
+
+		case 46: // Cyan
+			result |= bgRGBMask(0, 170, 170)
+
+		case 47: // White
+			result |= bgRGBMask(229, 229, 229)
+
+		case 100: // Bright Black (Gray)
+			result |= bgRGBMask(85, 85, 85)
+
+		case 101: // Bright Red
+			result |= bgRGBMask(255, 85, 85)
+
+		case 102: // Bright Green
+			result |= bgRGBMask(85, 255, 85)
+
+		case 103: // Bright Yellow
+			result |= bgRGBMask(255, 255, 85)
+
+		case 104: // Bright Blue
+			result |= bgRGBMask(85, 85, 255)
+
+		case 105: // Bright Magenta
+			result |= bgRGBMask(255, 85, 255)
+
+		case 106: // Bright Cyan
+			result |= bgRGBMask(85, 255, 255)
+
+		case 107: // Bright White
+			result |= bgRGBMask(255, 255, 255)
+
+		case 38: // foreground color
+			switch {
+			case len(values) > 4 && values[i+1] == 2:
+				result |= fgRGBMask(uint64(values[i+2]), uint64(values[i+3]), uint64(values[i+4]))
+				i += 4
+
+			case len(values) > 2 && values[i+1] == 5:
+				r, g, b := lookUp8bitColor(values[i+2])
+				result |= fgRGBMask(uint64(r), uint64(g), uint64(b))
+				i += 2
+
+			default:
+				return 0, fmt.Errorf("unsupported foreground color selection '%v'", values)
+			}
 
 		case 48: // background color
-			if i+4 >= len(values) {
-				return 0, fmt.Errorf("insufficient data to parse RGB background color")
-			}
+			switch {
+			case len(values) > 4 && values[i+1] == 2:
+				result |= bgRGBMask(uint64(values[i+2]), uint64(values[i+3]), uint64(values[i+4]))
+				i += 4
 
-			result |= bgMask
-			result |= uint64(values[i+2]) << 32
-			result |= uint64(values[i+3]) << 40
-			result |= uint64(values[i+4]) << 48
-			i += 4
+			case len(values) > 2 && values[i+1] == 5:
+				r, g, b := lookUp8bitColor(values[i+2])
+				result |= bgRGBMask(uint64(r), uint64(g), uint64(b))
+				i += 2
+
+			default:
+				return 0, fmt.Errorf("unsupported background color selection '%v'", values)
+
+			}
 		}
 	}
 
@@ -211,4 +316,16 @@ func processTextAnnotations(text *String) error {
 	}
 
 	return nil
+}
+
+func fgRGBMask(r, g, b uint64) uint64 {
+	return fgMask | r<<8 | g<<16 | b<<24
+}
+
+func bgRGBMask(r, g, b uint64) uint64 {
+	return bgMask | r<<32 | g<<40 | b<<48
+}
+
+func lookUp8bitColor(n uint8) (r, g, b uint8) {
+	return colorPalette8bit[n].RGB255()
 }
